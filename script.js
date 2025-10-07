a9b82e1484e2129b499ce15ddabcf1087585b902
sovanreachsor4-jpg <sovanreachsor4@gmail.com>
2025-10-07 21:44:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yayyyyyyy! I love you baby 💗💋";
+  gif.src = "https://i.pinimg.com/originals/17/78/bd/1778bd3bc3371e66373857531d78c2a2.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+  noBtn.style.position = "absolute";
+});
+
+
+
